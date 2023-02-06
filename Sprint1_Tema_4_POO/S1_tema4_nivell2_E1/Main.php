7969a7f5aca3f59bd78f4dd348bb018417065a5b
Levi Flores <levifv.madrid@gmail.com>
2023-02-06 09:44:25 +0100

    borrador exercici1 T4 de nivell2 de POO S1

--- Sprint1_Tema_4_POO/S1_tema4_nivell2_E1/Main.php
+++ Sprint1_Tema_4_POO/S1_tema4_nivell2_E1/Main.php
@@ -1,2 +1,31 @@
+<title>S1_tema4_nivell1_E2</title>
+<h1>Jugant als daus</h1><br/><br/>
+<?php
+require 'PokerDice.php';
+
+$Dau1 = new PokerDice();
+$Dau2 = new PokerDice();
+$Dau3 = new PokerDice();
+$Dau4 = new PokerDice();
+$Dau5 = new PokerDice();
+
+
+$Dau1->throw();
+$Dau2->throw();
+$Dau3->throw();
+$Dau4->throw();
+$Dau5->throw();
+
+
+
+echo "Dau 1: " . $Dau1->shapeName() . "<br/>";
+echo "Dau 2: " . $Dau2->shapeName() . "<br/>";
+echo "Dau 3: " . $Dau3->shapeName() . "<br/>";
+echo "Dau 4: " . $Dau4->shapeName() . "<br/>";
+echo "Dau 5: " . $Dau5->shapeName();
+
+echo "<br/>";
+echo "<br/>";
+echo "Total de llançaments: " . PokerDice::getTotalThrows();
 
 ?>
