ffd0888563bd341bb9f4eccf824c264be9dd7409
levifvy <levifv.madrid@gmail.com>
2023-03-14 19:26:42 +0100

    Subiendo cambio solicitados por el mentor s1_tema3_FiE_nivell1_E1

--- PHP_Sprint1_Tema 3_Funcions_i_Estructures/s1_tema3_FiE_nivell1_E1/workin.php
+++ PHP_Sprint1_Tema 3_Funcions_i_Estructures/s1_tema3_FiE_nivell1_E1/workin.php
@@ -1,18 +1,22 @@
 <?php
 
 if(isset($_GET['findnumber'])){
-    $n = $_GET['fnumber'];
+    $number = $_GET['fnumber'];
 }
 
-if ($n % 2 == 0) {
-    $resposta = "El numero ".$n." es parell.";
-}
-elseif ($n % 2 != 0) {
-    $resposta = "El numero ".$n." es imparell.";
-}
+function getEvensOrOdds($number){
+    if ($number % 2 == 0) {
+        $resposta = "El numero ".$number." es parell.";
+    }
+    elseif ($number % 2 != 0) {
+        $resposta = "El numero ".$number." es imparell.";
+    }
+    return $resposta;
+} 
+
 
 ?>
 <form action="workin.php" method="get">
 <label for="nfound"><h1>Resposta: </h1></label>
-<input type="text" style="background-color: #72D2F2" name="nfound" value="<?php echo $resposta ?>">
+<input type="text" style="background-color: #72D2F2" name="nfound" value="<?php echo getEvensOrOdds($number) ?>">
 </form>
