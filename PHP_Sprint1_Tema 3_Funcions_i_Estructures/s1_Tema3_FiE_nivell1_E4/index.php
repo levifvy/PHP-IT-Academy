0b42ff82af4d0d482eee826ac85fa8a65786e387
levifvy <levifv.madrid@gmail.com>
2023-03-14 22:33:36 +0100

    subiendo correccion s1_Tema3_FiE_nivell1_E4

--- PHP_Sprint1_Tema 3_Funcions_i_Estructures/s1_Tema3_FiE_nivell1_E4/index.php
+++ PHP_Sprint1_Tema 3_Funcions_i_Estructures/s1_Tema3_FiE_nivell1_E4/index.php
@@ -10,35 +10,36 @@
 <body style="background-color: #d1ccc0">
     <fieldset style="border: 2px #737495 solid;">
         <legend ><h2>Jugant a l'amagatall</h2></legend>
-        <p>Per prevenir oblits en utilitzar la nostra meravellosa opció "amagatall" establirem un paràmetre per defecte igual a 10 a la funció que s'encarrega de fer aquest compte.</p>
+        <p>Per prevenir oblits en utilitzar la nostra meravellosa opció "amagatall" establirem un paràmetre per 
+          defecte igual a 10 a la funció que s'encarrega de fer aquest compte.</p>
     </fieldset><br/><br/>
     <form action="index.php" method="get">
         <Label for="comptar" >Vull comptar fins: </Label> 
-        <input type="number" id="comptar" name="final" value="10" readonly placeholder="Ingressa un numero sencer">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+        <input type="number" id="comptar" name="final" value="" placeholder="Ingressa un numero sencer" required>
         <input style="border: 2px #737495 solid;" type="submit" name="boton" value="Comptar"><br/>
     </form><br/><br/>
-    <hr style="border: 2px #737495 solid;"><br/>
-
+    <hr style="border: 2px #737495 solid;">
     <?php
     if(isset($_GET['boton'])){
-    $n = $_GET['final'];
+    $number = $_GET['final'];
     }
-    
-    function countTwo($n) {
-        for ($i = 2; $i <= $n; $i++) {
-          if ($i % 2 == 0) {
-            echo $i . "   ";
-          }
+    function countTwoAndTwo($number) {
+      for ($i = 2; $i <= $number; $i+=2){
+        echo $i . "   ";
         }
+    }
+    function countTwoAndTwoGame($number){
+      echo "1  ";
+      if ($number % 2 == 0) {
+        echo countTwoAndTwo($number);    
+      }elseif ($number % 2 != 0) {
+        echo countTwoAndTwo($number)." ".$number;
       }
-
-  
-    
+    }
     ?>
-    <form action="index.php" method="get">
-        <h2>Resposta: </h2><br/>
-        <input class="box" type="text" name="gotit" 
-        value="<?php echo countTwo($n); ?>">
-    </form>
+    <h2>Resposta: </h2>
+    <div class="box">
+      <?php echo countTwoAndTwoGame($number); ?>
+      </div>
 </body>
 </html>
