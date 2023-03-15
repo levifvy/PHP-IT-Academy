b30c025807c32496443f0b55b5478ab2cf202a70
levifvy <levifv.madrid@gmail.com>
2023-03-15 10:49:09 +0100

    subiendo correccion s1_tema3_FiE_nivell1_E1

--- PHP_Sprint1_Tema 3_Funcions_i_Estructures/s1_tema3_FiE_nivell1_E1/workin.php
+++ PHP_Sprint1_Tema 3_Funcions_i_Estructures/s1_tema3_FiE_nivell1_E1/workin.php
@@ -1,20 +1,16 @@
 <?php
-
 if(isset($_GET['findnumber'])){
     $number = $_GET['fnumber'];
 }
-
 function getEvensOrOdds($number){
     if ($number % 2 == 0) {
         $resposta = "El numero ".$number." es parell.";
     }
-    elseif ($number % 2 != 0) {
+    else {
         $resposta = "El numero ".$number." es imparell.";
     }
     return $resposta;
 } 
-
-
 ?>
 <form action="workin.php" method="get">
 <label for="nfound"><h1>Resposta: </h1></label>
