cdeb6dc28f7fee76fe2318a36ba8b8c054c23278
levifvy <levifv.madrid@gmail.com>
2023-06-09 02:50:44 +0200

    adding mysql structures sprint2 topic 1

--- Sprint2_T1_MySQL_Estructura_de_dades/optica_excercicis_de_verificacion.sql
+++
@@ -1,9 +0,0 @@
--- Base de dades Optica
--- 1. Llista el total de compres d’un client/a.
-SELECT SUM(g.precio) AS total_compras FROM gafas g INNER JOIN ventas v ON g.id_gafa = v.id_gafa;
-
--- 2. Llista les diferents ulleres que ha venut un empleat durant un any.
-SELECT * FROM gafas g JOIN ventas v ON g.id_gafa = v.id_gafa JOIN empleado e ON v.id_venta = e.id_venta  WHERE YEAR(v.fecha_venta) = YEAR(CURDATE());
-  
--- 3. Llista els diferents proveïdors que han subministrat ulleres venudes amb èxit per l'òptica.
-SELECT DISTINCT p.nombre AS Nom_Proveidor, m.nombre AS Marc FROM proveedores p JOIN marcas m ON p.id_proveedor = m.id_proveedor JOIN gafas g ON g.id_marca = m.id_marca JOIN ventas v ON g.id_gafa = v.id_gafa;
